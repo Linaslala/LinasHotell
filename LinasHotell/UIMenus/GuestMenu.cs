826ed30e8711727c0ddb8e9666b0620775fc388d
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-04-17 13:52:40 +0200

    Fyllt i gästmeny

--- LinasHotell/UIMenus/GuestMenu.cs
+++ LinasHotell/UIMenus/GuestMenu.cs
@@ -1,10 +1,65 @@
-﻿using System;
+﻿using LinasHotell.Controllers;
+using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LinasHotell.UIMenus
 {
-    internal class GuestMenu
+    public class GuestMenu
     {
+        private readonly GuestController _guestController;
+
+        public GuestMenu(GuestController guestController)
+        {
+            _guestController = guestController;
+        }
+
+
+        public async Task ShowGuestMenuAsync()
+        {
+            while (true)
+            {
+                var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Gästemeny")
+                    .AddChoices(
+                        "Visa alla gäster",
+                        "Registrera ny gäst",
+                        "Uppdatera gästinformation",
+                        "Checka in/ut gäst",
+                        "Tillbaka till lobbyn"));
+
+                switch (choice)
+                {
+                    case "Visa alla gäster":
+                        await _guestController.ShowAllGuestsAsync();
+                        break;
+
+                    case "Registrera ny gäst":
+                        await _guestController.AddGuestAsync();
+                        break;
+
+                    case "Uppdatera gästinformation":
+                        await _guestController.UpdateGuestAsync();
+                        break;
+
+                    case "Checka in/ut gäst":
+                        await _guestController.SetGuestStatusAsync();
+                        break;
+
+                    case "Tillbaka till lobbyn":
+                        return;
+                }
+            }
+
+        }
+    }
+
+    public enum GuestMenuResult
+    {
+        Stay,
+        BackToLobby
     }
 }
+
