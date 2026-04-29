732032fbc17268253d700008e0ef7f6e2e0e0b66
Linaslala <lina.sk.samuelsson@hotmail.com>
2026-04-29 09:33:23 +0200

    Remove comment syntax from SQL queries

--- syntax.sql
+++ syntax.sql
@@ -1,25 +1,25 @@
-# -- Hämta alla gäster --
-# SELECT * 
-# FROM Guests
+-- Hämta alla gäster --
+SELECT * 
+FROM Guests
 
-# -- Hämta förnamn och efternamn på gäster --
-# SELECT FirstName, LastName
-# FROM Guests
+-- Hämta förnamn och efternamn på gäster --
+SELECT FirstName, LastName
+FROM Guests
 
-# -- Hämta alla rum som kostar mer än 1000 kr per natt --
-# SELECT *
-# FROM Rooms
-# WHERE PricePerNight > 1000
+-- Hämta alla rum som kostar mer än 1000 kr per natt --
+SELECT *
+FROM Rooms
+WHERE PricePerNight > 1000
 
-# -- Hämta alla bokningar sorterade efter startdatum (senaste först) --
-# SELECT *
-# FROM Bookings
-# ORDER BY CheckInDate DESC
+-- Hämta alla bokningar sorterade efter startdatum (senaste först) --
+SELECT *
+FROM Bookings
+ORDER BY CheckInDate DESC
 
-# -- Räkna hur många rum som finns totalt --
-# SELECT COUNT(*) AS TotalNumberOfRooms
-# FROM Rooms
+-- Räkna hur många rum som finns totalt --
+SELECT COUNT(*) AS TotalNumberOfRooms
+FROM Rooms
 
-# -- Visa unika rumstyper --
-# SELECT DISTINCT RoomType
-# FROM Rooms
+-- Visa unika rumstyper --
+SELECT DISTINCT RoomType
+FROM Rooms
